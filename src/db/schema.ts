edd2a5114910d9a53f203d3380e12b38b33cea97
Thomas Soares <thomas__soares@hotmail.com>
2024-10-08 23:30:08 -0300

    Finish schema.

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -3,3 +3,7 @@ import { pgTable, text, integer, timestamp } from 'drizzle-orm/pg-core'
 export const goals = pgTable('goals', {
     id: text('id').primaryKey(),
 })
+
+export const goalCompletions = pgTable('goal_completion', {
+    id: text('id').primaryKey(),
+})
