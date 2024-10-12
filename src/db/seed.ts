d48eecef184c73ce1d1a77559a07e7bbc057f256
Thomas Soares <thomas__soares@hotmail.com>
2024-10-11 23:41:17 -0300

    wip seed

--- src/db/seed.ts
+++ src/db/seed.ts
@@ -2,7 +2,12 @@ import { db } from "."
 import { goalCompletions, goals } from "./schema"
 
 async function seed() {
-    await
+    await db.delete(goalCompletions)
+    await db.delete(goals)
+
+    await db.insert(goals).values({
+        
+    })
 }
 
 seed()
