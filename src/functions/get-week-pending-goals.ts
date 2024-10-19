4407e3ff607163a86ba28e3f8b4e76ddebc10c41
Thomas Soares <thomas__soares@hotmail.com>
2024-10-18 23:53:34 -0300

    get goals

--- src/functions/get-week-pending-goals.ts
+++ src/functions/get-week-pending-goals.ts
@@ -1,1 +1,8 @@
 import { goalCompletions, goals } from "../db/schema";
+
+
+dayjs.extend(weekOfYear)
+
+export async function getWeekPendingGoals() {
+    const firstDayOfWeek = dayjs().startOf('week')
+}
