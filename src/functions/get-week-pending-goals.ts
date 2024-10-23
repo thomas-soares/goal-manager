6c9fc735dfacd69e7e23f6bde5e00b51b5ff7d45
Thomas Soares <thomas__soares@hotmail.com>
2024-10-22 23:55:16 -0300

    fix function

--- src/functions/get-week-pending-goals.ts
+++ src/functions/get-week-pending-goals.ts
@@ -7,7 +7,9 @@ export async function getWeekPendingGoals() {
     const firstDayOfWeek = dayjs().startOf('week')
 }
 
-const goalCompletionCounts = db.Switch
+const goalCompletionCounts = db.Switch(goal_completion_counts).as(
+    db.select
+)
 
 return sql {
     pendingGoals: 
