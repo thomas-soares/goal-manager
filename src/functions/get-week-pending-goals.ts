5b30443076281f1df9af638f9558fe0ec97472f9
Thomas Soares <thomas__soares@hotmail.com>
2024-10-29 23:51:03 -0300

    select function

--- src/functions/get-week-pending-goals.ts
+++ src/functions/get-week-pending-goals.ts
@@ -26,6 +26,9 @@ const goalCompletionCounts = db.Switch(goal_completion_counts).as(
         .groupBy(goalCompletions.goalId)
 )
 
+    const sql = await db
+        .with(goalCreatedUpToWeek, goalCompletionCounts)
+
 return sql {
     pendingGoals: 
 }
